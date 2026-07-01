b8c9dcd140849812dd9fdc2634051c66951d891b
DarioM1992 <dario21992@gmail.com>
2026-07-01 19:56:07 +0200

    creato footer top con div per ogni sezione di links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,36 +2,66 @@ import './Footer.css';
 
 const BigLogo = () => (
     <img className='BigLogo' src="img/dc-logo-bg.png" alt="logo-DC" />
-)
+);
+
+const FooterTop = () => (
+    <div className='container'>
+        <div className='footer-col'>
+            <h4>DC COMICS</h4>
+            <ul>
+                <li><a href="#">Characters</a></li>
+                <li><a href="#">Comics</a></li>
+                <li><a href="#">Movies</a></li>
+                <li><a href="#">TV</a></li>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">Videos</a></li>
+                <li><a href="#">News</a></li>
+            </ul>
+        
+            <h4>SHOP</h4>
+            <ul>
+                <li><a href="#">Shop DC</a></li>
+                <li><a href="#">Shop DC Collectibles</a></li>
+            </ul>
+            
+        </div>
+         <div className='footer-col'>
+            <h4>DC</h4>
+            <ul>
+            <li><a href="#">Terms Of Use</a></li>
+            <li><a href="#">Privacy policy (New)</a></li>
+            <li><a href="#">Ad Choices</a></li>
+            <li><a href="#">Advertising</a></li>
+            <li><a href="#">Jobs</a></li>
+            <li><a href="#">Subscriptions</a></li>
+            <li><a href="#">Talent Workshops</a></li>
+            <li><a href="#">CPSC Certificates</a></li>
+            <li><a href="#">Ratings</a></li>
+            <li><a href="#">Shop Help</a></li>
+            <li><a href="#">Contact Us</a></li>
+            </ul>
+        </div>
+        <div className='footer-col'>
+            <h4>SITES</h4>
+            <ul>
+              <li><a href="#">DC</a></li>
+              <li><a href="#">MAD Magazine</a></li>
+              <li><a href="#">DC Kids</a></li>
+              <li><a href="#">DC Universe</a></li>
+              <li><a href="#">DC Power Visa</a></li>
+            </ul>            
+        
+        </div>
+    </div>
+    
+);
 
 export const Footer = () => (
     <footer>
-        <section className="DC-COMICS">
-            <h3>DC COMICS</h3>
-            <ul>
-                <li><a href=""></a></li>
-                <li><a href=""></a></li>
-                <li><a href=""></a></li>
-                <li><a href=""></a></li>
-                <li><a href=""></a></li>
-                <li><a href=""></a></li>
-                <li><a href=""></a></li>
-            </ul>
-
-        </section>
-        <section className='SHOP'>
-            <h3>SHOP</h3>
-
-        </section>
-        <section className="DC">
-            <h3>DC</h3>
-
-        </section>
-        <section className="SITES">
-            <h3>SITES</h3>
-
-        </section>
         <BigLogo />
+        <FooterTop />
+        
+        
     </footer>
     
 );

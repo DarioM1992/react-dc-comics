48f6b9d98c936722b8dc080e4f0d20608cb10f58
DarioM1992 <dario21992@gmail.com>
2026-07-01 16:50:51 +0200

    inserito logo e navbar nell'header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,25 @@
+const Logo = () => (
+    <img src="img/dc-logo.png" alt="logo-DC" />
+)
+
+const Nav = () => (
+    <ul>
+        <li><a href="">characters</a></li>
+        <li><a href="">comics</a></li>
+        <li><a href="">movies</a></li>
+        <li><a href="#">tv</a></li>
+        <li><a href="">games</a></li>
+        <li><a href="">collectibles</a></li>
+        <li><a href="">videos</a></li>
+        <li><a href="">fans</a></li>
+        <li><a href="">news</a></li>
+        <li><a href="">shop</a></li>
+    </ul>
+)
+
 export const Header = () => (
-    <>
-    </>
+    <header>
+        <Logo />
+        <Nav />
+    </header>
 );

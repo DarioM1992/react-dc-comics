2655cfda9c61fa4c8d152114e30333460b0edb32
DarioM1992 <dario21992@gmail.com>
2026-07-01 17:01:52 +0200

    sistemato un po' di cose

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,16 +5,16 @@ const Logo = () => (
 const Nav = () => (
     <ul>
         <li>
-            <a href="">characters</a>
-            <a href="">comics</a>
-            <a href="">movies</a>
+            <a href="#">characters</a>
+            <a href="#" className="active">comics</a>
+            <a href="#">movies</a>
             <a href="#">tv</a>
-            <a href="">games</a>
-            <a href="">collectibles</a>
-            <a href="">videos</a>
-            <a href="">fans</a>
-            <a href="">news</a>
-            <a href="">shop</a>
+            <a href="#">games</a>
+            <a href="#">collectibles</a>
+            <a href="#">videos</a>
+            <a href="#">fans</a>
+            <a href="#">news</a>
+            <a href="#">shop</a>
         </li>
     </ul>
 )
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,4 @@
 export const Main = () => (
-    <>
-    </>
+    <main>
+    </main>
 );

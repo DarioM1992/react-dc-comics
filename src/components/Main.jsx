a3d223612e628f3cdec708e156766ca27f31c488
DarioM1992 <dario21992@gmail.com>
2026-07-01 18:34:13 +0200

    sistemata nav in header e posizione delle a in main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,8 +6,8 @@ export const Main = () => (
         <section className='extra'>
                 <a href="#"><img src="./img/buy-comics-digital-comics.png" alt="" /><span>digital comics</span></a>
                 <a href="#"><img src="./img/buy-comics-merchandise.png" alt="" /><span>dc merchandise</span></a>
-                <a href="#"><img src="./img/buy-comics-shop-locator.png" alt="" /><span>subscription</span></a>
-                <a href="#"><img src="./img/buy-comics-subscriptions.png" alt="" /><span>comic shop locator</span></a>
+                <a href="#"><img src="./img/buy-comics-subscriptions.png" alt="" /><span>subscription</span></a>
+                <a href="#"><img src="./img/buy-comics-shop-locator.png" alt="" /><span>comic shop locator</span></a>
                 <a href="#"><img src="./img/buy-dc-power-visa.svg" alt="" /><span>dc power visa</span></a>
         </section>
         

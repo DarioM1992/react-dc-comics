67c7b6654bbecbd8c453b1f51af5afd3e126523c
DarioM1992 <dario21992@gmail.com>
2026-07-01 16:42:25 +0200

    inseriti assets di img e fatto import export compjsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
+import { Header } from './components/Header';
+import { Main } from './components/Main';
+import { Footer } from './components/Footer';
 
 const App = () => (
   <>
-  <Header />
+  <Header/>
   <Main />
   <Footer />
   </>

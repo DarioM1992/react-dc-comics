9c19f3e2f5868636e8136f1cd10e39b81d214bfb
DarioM1992 <dario21992@gmail.com>
2026-07-01 18:02:15 +0200

    inserito main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,5 +2,16 @@ import './Main.css';
 
 export const Main = () => (
     <main>
+        <div className='content'><p> {'-->'} Content goes here {'<--'}</p></div>
+        <div >
+            <ul className='extra'>
+                <li><a href="#">digital comics</a></li>
+                <li><a href="#">dc merchandise</a></li>
+                <li><a href="#">subscription</a></li>
+                <li><a href="#">comic shop locator</a></li>
+                <li><a href="#">dc power visa</a></li>
+            </ul>
+        </div>
+        
     </main>
 );

f3b7b60ace73f2a9210446c9d79483242ec40010
DarioM1992 <dario21992@gmail.com>
2026-07-01 19:02:12 +0200

    creazione footer 1°fase

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,6 +6,31 @@ const BigLogo = () => (
 
 export const Footer = () => (
     <footer>
+        <section className="DC-COMICS">
+            <h3>DC COMICS</h3>
+            <ul>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+            </ul>
+
+        </section>
+        <section className='SHOP'>
+            <h3>SHOP</h3>
+
+        </section>
+        <section className="DC">
+            <h3>DC</h3>
+
+        </section>
+        <section className="SITES">
+            <h3>SITES</h3>
+
+        </section>
         <BigLogo />
     </footer>
     

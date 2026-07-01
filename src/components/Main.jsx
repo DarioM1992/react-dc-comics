0151756e3916a690c7b0f5c1270478b22c140e04
DarioM1992 <dario21992@gmail.com>
2026-07-01 18:21:18 +0200

    sistemando icone e span

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,15 +3,13 @@ import './Main.css';
 export const Main = () => (
     <main>
         <div className='content'><p> {'-->'} Content goes here {'<--'}</p></div>
-        <div >
-            <ul className='extra'>
-                <li><a href="#">digital comics</a></li>
-                <li><a href="#">dc merchandise</a></li>
-                <li><a href="#">subscription</a></li>
-                <li><a href="#">comic shop locator</a></li>
-                <li><a href="#">dc power visa</a></li>
-            </ul>
-        </div>
+        <section className='extra'>
+                <a href="#"><img src="./img/buy-comics-digital-comics.png" alt="" /><span>digital comics</span></a>
+                <a href="#"><img src="./img/buy-comics-merchandise.png" alt="" /><span>dc merchandise</span></a>
+                <a href="#"><img src="./img/buy-comics-shop-locator.png" alt="" /><span>subscription</span></a>
+                <a href="#"><img src="./img/buy-comics-subscriptions.png" alt="" /><span>comic shop locator</span></a>
+                <a href="#"><img src="./img/buy-dc-power-visa.svg" alt="" /><span>dc power visa</span></a>
+        </section>
         
     </main>
 );

aae9c6e9949bbebb85142da727394e4c91f2c1cc
DarioM1992 <dario21992@gmail.com>
2026-07-01 18:53:58 +0200

    sistemando sfondo e logo in absolute sul footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,12 @@
 import './Footer.css';
 
+const BigLogo = () => (
+    <img className='bigLogo' src="img/dc-logo-bg.png" alt="logo-DC" />
+)
+
 export const Footer = () => (
     <footer>
+        <BigLogo />
     </footer>
+    
 );
